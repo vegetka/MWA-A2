dfd2cd86bb9e030a3410964ecb0f341b8c596712
vegetka <vegetka@gmail.com>
2017-10-13 16:01:09 +0100

    All looks ugly, but works.

--- modules/myModule.js
+++ modules/myModule.js
@@ -1,3 +1,5 @@
+angular.module('mwaa2', ['ngMaterial']);
+
 angular.module('mwaa2', ['ngRoute']).
     config(function($routeProvider){
         $routeProvider.
@@ -11,15 +13,21 @@ function MainController($scope){
 }
 
 function HomeController($scope){
-    console.log("inside HomeController");
+    // debug
+    l("inside HomeController");
     $scope.title = "Do not push the putton";
     $scope.body  = "Welcome";
-    $scope.list  = "List";
+    $scope.listInfo = "List will show below (press X to delete item)";
     $scope.item  = [];
     $scope.button = "Add";
+    // add new element to the list
     $scope.addElement = function(){
+        // get text from input box, add item to the list
         $scope.item.push({'new': $scope.newItem, 'done':false});
+        // clear input field
+        $scope.newItem = "";
     }
+    // remove element from the list
     $scope.removeElement = function(index){
         $scope.item.splice(index, 1);
     };
@@ -30,7 +38,7 @@ function FAQcontroller($scope){
     $scope.body  = "Fequently Asked Questions";
 }
 
-function Clicker($scope){
-    $scope.item = "two";
-    console.log("inside clicker");
+// debugger
+function l(s){
+    console.log(s);
 }
